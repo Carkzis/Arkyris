d3ba8ff4a073ca256c63e5d4d76c15751c45cf02
Carkzis <marc.jowett@gmail.com>
2021-03-27 17:14:59 +0000

    Added simple add text method to diary, added saved states.

--- app/src/main/java/com/example/arkyris/DiaryActivity.java
+++ app/src/main/java/com/example/arkyris/DiaryActivity.java
@@ -2,8 +2,11 @@ package com.example.arkyris;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +16,10 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class DiaryActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = DiaryActivity.class.getSimpleName();
+    public static final String EXTRA_PUBLICISE = "com.example.android.Arkyris.extra.PUBLICISE";
+    private EditText mReply;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,19 @@ public class DiaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diary);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Initiate attributes
+        mReply = findViewById(R.id.editText_publicise);
+
+        // This will only do something if the Send button was received
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+        if (message != null){
+            TextView headerTextView = findViewById(R.id.text_header);
+            headerTextView.setText(R.string.message_received);
+        }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -43,10 +63,57 @@ public class DiaryActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_diary, menu);
         return true;
+    }
+
+    public void returnReply(View view) {
+        Log.d(LOG_TAG, "Publicise button clicked!");
+        String publicise = mReply.getText().toString();
+        Intent publiciseIntent = new Intent(); // don't reuse intent used to get to the Diary
+        publiciseIntent.putExtra(EXTRA_PUBLICISE, publicise);
+        setResult(RESULT_OK, publiciseIntent);
+        Log.d(LOG_TAG, "End DiaryActivity");
+        finish(); // closes the activity and returns to the main activity
     }
 }
--- app/src/main/java/com/example/arkyris/MainActivity.java
+++ app/src/main/java/com/example/arkyris/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,20 +13,86 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.example.android.Arkyris.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+    private EditText mMessageEditText;
     private int mCount = 0;
     private TextView mShowCount;
     private Button mZeroButton;
     private Button mCountButton;
+    private TextView mPublicTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(LOG_TAG, "Let us begin!");
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
+        // Initialise view variables
         mShowCount = (TextView) findViewById(R.id.show_count);
         mZeroButton = (Button) findViewById(R.id.button_zero);
         mCountButton = (Button) findViewById(R.id.button_count);
+        mMessageEditText = findViewById(R.id.editText_main);
+        mPublicTextView = findViewById(R.id.text_public_message);
+
+        // Restore the state
+        if (savedInstanceState != null) {
+            // if the public text is visible, set the textview with the saved text
+            boolean isVisible = savedInstanceState.getBoolean("public_text_visible");
+            if (isVisible){
+                mPublicTextView.setText(savedInstanceState.getString("public_text"));
+                mPublicTextView.setVisibility(View.VISIBLE);
+            }
+            mShowCount.setText(savedInstanceState.getString("show_count"));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("show_count", mShowCount.getText().toString());
+        if (mPublicTextView.getVisibility() == View.VISIBLE){
+            outState.putBoolean("public_text_visible", true);
+            outState.putString("public_text", mPublicTextView.getText().toString());
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
     }
 
     public void showToast(View view) {
@@ -66,4 +133,27 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DiaryActivity.class);
         startActivity(intent);
     }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Send button clicked!");
+        Intent intent = new Intent(this, DiaryActivity.class);
+        String message = mMessageEditText.getText().toString();
+        mMessageEditText.getText().clear();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // test that the correct Intent is processed
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String publicisedMessage = data.getStringExtra(DiaryActivity.EXTRA_PUBLICISE);
+                mPublicTextView.setText(publicisedMessage);
+                mPublicTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
 }
