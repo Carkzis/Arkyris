04634a232f9c1fbd63c0f704b583e52d76c2b153
Carkzis <marc.jowett@gmail.com>
2021-04-30 21:59:46 +0100

    Added change password activity

---
+++ app/src/main/java/com/example/arkyris/ChangePasswordActivity.java
@@ -0,0 +1,17 @@
+package com.example.arkyris;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+
+
+
+}
