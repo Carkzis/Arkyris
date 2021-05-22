f4bb7ea2c967491b0493ff443764c320e7048e8d
Carkzis <marc.jowett@gmail.com>
2021-05-22 18:57:19 +0100

    Now connects to heroku api

--- app/src/main/java/com/example/arkyris/APIUtils.java
+++ app/src/main/java/com/example/arkyris/APIUtils.java
@@ -8,7 +8,7 @@ public class APIUtils {
     public APIUtils() {
     }
 
-    public static final String API_URL = "http://192.168.0.14:8000/arkyris_api/";
+    public static final String API_URL = "http://arkyris.herokuapp.com/arkyris_api/";
 
     public static EntryService getEntryService() {
         return RetrofitClient.getClient(API_URL).create(EntryService.class);
