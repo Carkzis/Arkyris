1dd348d4ff0332df9e6cd9d51e535a8790e276e8
Carkzis <marc.jowett@gmail.com>
2021-09-24 23:55:33 +0100

    Amended drf naming

--- arkyris_backend/accounts/urls.py
+++ arkyris_backend/accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import RegisterAPI, LoginAPI, UserAPI, ChangePasswordView
+from .views import RegisterAPI, LoginAPI, UserAPI, ChangePasswordAPI
 from django.urls import path, include
 from knox import views as knox_views
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
+    path('change_password/', ChangePasswordAPI.as_view(), name='change_password'),
     path('user/', UserAPI.as_view(), name='user'),
 ]
